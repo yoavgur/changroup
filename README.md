e917b652241c11006610d52371b9bad53be293f6
Yoav Gur Arieh <yoav@lmnr.io>
2021-05-22 23:44:55 +0300

    added doonce and readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# changroup
+# Changroup
+Changroup is a minimal implemenation of sync.WaitGroup that allows you to call `Wait()` in a non-blocking manner, by exposing it through a channel.
+The purpose of this package is simplify situations where `Wait()` should be performed with a timeout, or when other operations should be carried out
+simultaneously (through the use of select).
--- changroup.go
+++ changroup.go
@@ -4,15 +4,19 @@ import "sync"
 
 type ChanGroup struct {
 	sync.WaitGroup
+	wait   chan struct{}
+	doOnce sync.Once
 }
 
 func (cg *ChanGroup) WaitCh() <-chan struct{} {
-	wait := make(chan struct{})
+	cg.doOnce.Do(func() {
+		cg.wait = make(chan struct{})
 
-	go func() {
-		cg.Wait()
-		close(wait)
-	}()
+		go func() {
+			cg.Wait()
+			close(cg.wait)
+		}()
+	})
 
-	return wait
+	return cg.wait
 }
