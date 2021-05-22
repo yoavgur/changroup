bfce30e7eb75fa42cd90cba65ff9b0c401c17659
Yoav Gur Arieh <yoav@lmnr.io>
2021-05-22 23:49:19 +0300

    added function doc

--- changroup.go
+++ changroup.go
@@ -8,6 +8,10 @@ type ChanGroup struct {
 	doOnce sync.Once
 }
 
+// WaitCh returns a channel which will be closed when the waitgroup counter is zero
+// When called it creates a single goroutine which waits for the waitgroup and closes the channel.
+// This function can be called multiple times, but once the waitgroup has finished waiting
+// re-calling this function will always return a closed channel. Thus you cannot reuse a finished waitgroup.
 func (cg *ChanGroup) WaitCh() <-chan struct{} {
 	cg.doOnce.Do(func() {
 		cg.wait = make(chan struct{})
